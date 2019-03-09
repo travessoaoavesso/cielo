f11f48a17e8bf4552d49bc25364c6dce897e3310
travessoaoavesso <48385298+travessoaoavesso@users.noreply.github.com>
2019-03-09 12:51:40 -0300

    Update calcula-pagamentos.js

--- javascripts/calcula-pagamentos.js
+++ javascripts/calcula-pagamentos.js
@@ -297,6 +297,18 @@ function validarCamposObrigatorios(dataVenda, valorVenda){
 		retorno = false;
 	}
 	alert(valorVenda);
+	if(valorVenda is undefined){
+		alert("is undefined");
+	}
+	if(valorVenda == undefined){
+		alert("== undefined");
+	}
+	if(valorVenda is null){
+		alert"is null");
+	}
+	if(valorVenda isNull){
+		alert("isNull);
+	}
 	if(valorVenda <= 0){
 		alert("Valor da Venda inválido!");
 		retorno = false;
